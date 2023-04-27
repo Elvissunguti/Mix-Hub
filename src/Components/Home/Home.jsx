06747eaa657b5b7d6d214d473d1abeb4909b60de
Elvis Sunguti <elviskendrick99@gmail.com>
2023-04-27 16:19:14 +0300

    Initial commit

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBar from "../Navbar/NavBar";
+
+const Home = () => {
+    return(
+       <>
+       <NavBar />
+       <Hero />
+       </>
+    )
+}
+export default Home;
